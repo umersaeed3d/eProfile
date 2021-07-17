68d9fdfafe3ed98c53dabe21004b5a1c2bcac6e9
Muhammad Umer Saeed <umersaeed3d@gmail.com>
2021-07-17 17:56:54 +0500

    Initial commit

--- vendor/composer/autoload_real.php
+++ vendor/composer/autoload_real.php
@@ -13,19 +13,24 @@ class ComposerAutoloaderInitab435849548cde8b833a6fe999a056e3
         }
     }
 
+    /**
+     * @return \Composer\Autoload\ClassLoader
+     */
     public static function getLoader()
     {
         if (null !== self::$loader) {
             return self::$loader;
         }
 
+        require __DIR__ . '/platform_check.php';
+
         spl_autoload_register(array('ComposerAutoloaderInitab435849548cde8b833a6fe999a056e3', 'loadClassLoader'), true, true);
-        self::$loader = $loader = new \Composer\Autoload\ClassLoader();
+        self::$loader = $loader = new \Composer\Autoload\ClassLoader(\dirname(\dirname(__FILE__)));
         spl_autoload_unregister(array('ComposerAutoloaderInitab435849548cde8b833a6fe999a056e3', 'loadClassLoader'));
 
         $useStaticLoader = PHP_VERSION_ID >= 50600 && !defined('HHVM_VERSION') && (!function_exists('zend_loader_file_encoded') || !zend_loader_file_encoded());
         if ($useStaticLoader) {
-            require_once __DIR__ . '/autoload_static.php';
+            require __DIR__ . '/autoload_static.php';
 
             call_user_func(\Composer\Autoload\ComposerStaticInitab435849548cde8b833a6fe999a056e3::getInitializer($loader));
         } else {
